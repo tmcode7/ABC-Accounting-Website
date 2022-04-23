e42c9b443ce2435f6bac8400002fef7b0028ce54
tmcode7 <mawuenatord7@gmail.com>
2022-04-23 17:21:08 +0000

    added hero section

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('IMG_1558.jpg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
